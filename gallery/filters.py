9ca228f348c88df12cb89fa2da7683c618e5d155
Navanjane <nkindelpitiya@gmail.com>
2020-07-04 17:14:51 +0530

    Search By name Added4

--- gallery/filters.py
+++ gallery/filters.py
@@ -1,8 +1,11 @@
 import django_filters
+from django_filters import CharFilter
 
 from .models import *
 
 class Filter(django_filters.FilterSet):
+    paint_name = CharFilter(field_name='paint_name', lookup_expr='icontains')
+
     class Meta:
         model = Paint
         fields = ['category']
